4c5f3e201a1d3b90ce50c139fc2bcc5e39d99450
3ul0gin <eloy_234@hotmail.com>
2023-05-07 13:25:20 -0500

    Creando proyecto html con CSS y Js

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function activaScroll(selector) {
+    var button = document.querySelector(selector);
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        var page = document.querySelector('html');
+        var origen = Math.max(page.scrollTop);
+        console.log(origen);
+        var destino = document.querySelector(this.attributes.href.value).offsetTop;
+        console.log(destino);
+        var counter = 0;
+
+        var animation = setInterval(function() {
+            counter++;
+            console.log(counter);
+            page.scrollTop = origen + (destino*counter*0.1);
+            console.log(origen + (destino*counter*0.1));
+        },50);
+
+        setTimeout(function() {
+            clearInterval(animation);
+        },500);
+    });
+}
+
+activaScroll('a[href*=about]');
+activaScroll('a[href*=speakers]');
+activaScroll('a[href*=contacto]');
